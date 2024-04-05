ba9acba99ca4f828d0d5817e6721e97a1758fc89
8aChristian <145133332+8aChristian@users.noreply.github.com>
2024-04-05 09:25:34 -0500

    Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Database, object, ref, update } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  temperatureValue: number = 0;
+  temperaturePercentage: number = 0;
 
-  constructor() {}
+  constructor(private database: Database) {}
 
+  ngOnInit() {
+    const route = ref(this.database, 'Termometro');
+    object(route).subscribe((attributes) => {
+      const temperature = attributes.snapshot.val();
+      this.updateTemperature(temperature);
+    });
+  }
+
+  updateTemperature(temperature: number) {
+    this.temperatureValue = Math.round(this.convertTocelsius(temperature));
+    this.temperaturePercentage = (this.temperatureValue / 100) * 100;
+  }
+  
+  convertTocelsius(temperature: number): number {
+    return Math.round((temperature * 100) / 255);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDrH7TDd9JtoDjRVOoLvuC_GzZgLapC6qs",
+    authDomain: "rtdb-termometer.firebaseapp.com",
+    projectId: "rtdb-termometer",
+    storageBucket: "rtdb-termometer.appspot.com",
+    messagingSenderId: "579896166118",
+    appId: "1:579896166118:web:0258a9ddac4b5527addd27"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
